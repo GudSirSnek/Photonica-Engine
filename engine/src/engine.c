f83da90f08048d25397283951f092019435611b3
Timothy Mackie <llolcake95@gmail.com>
2023-04-24 01:18:49 +0200

    s

--- engine/src/engine.c
+++ engine/src/engine.c
@@ -1,65 +1,12 @@
 #include <engine.h>
 #include <stdio.h>
 #include <math.h>
-static SDL_Window *window = NULL;
-static SDL_GLContext maincontext;
-uint32_t w_flags = 0;
-GLuint shader_default;
-
-
 
 
 primitive pe_rect = {0,0,0,12,6};
 
-void pe_init_rect(unsigned int *vao, unsigned int *vbo, unsigned int *ebo){
-    //	 x,	y, z, u, v
-	float vertices[] = {
-		 0.5,  0.5, 0,
-		 0.5, -0.5, 0,
-		-0.5, -0.5, 0,
-		-0.5,  0.5, 0,
-	};
-
-	unsigned int indices[] = {
-		0, 1, 3,
-		1, 2, 3
-	};
-
-	glGenVertexArrays(1, vao);
-	glGenBuffers(1, vbo);
-	glGenBuffers(1, ebo);
-
-	glBindVertexArray(*vao);
-
-	glBindBuffer(GL_ARRAY_BUFFER, *vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// xyz
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0); 
-
-	glBindVertexArray(0);
-}
 
 void pe_init(void){
-    /*
-    if (SDL_Init(SDL_INIT_VIDEO) != 0){
-        pe_printError("Failed to Initialize SDL: %s", SDL_GetError());
-    }
-
-    // Request an OpenGL 4.5 context (should be core)
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
-    // Also request a depth buffer
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    pe_printInfo("successfully initialized Photonica Engine", NULL);
-    */
    // Initialize SDL 
     if (SDL_Init(SDL_INIT_VIDEO) != 0){
         pe_printError("Failed to Initialize SDL: %s", SDL_GetError());
@@ -159,6 +106,40 @@ void pe_endRender(void){
 void pe_drawCircle(float cx, float cy, float r, int num_segments)
 {
 
+}
+
+void pe_init_rect(unsigned int *vao, unsigned int *vbo, unsigned int *ebo){
+    //	 x,	y, z, u, v
+	float vertices[] = {
+		 0.5,  0.5, 0,
+		 0.5, -0.5, 0,
+		-0.5, -0.5, 0,
+		-0.5,  0.5, 0,
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+
+	glGenVertexArrays(1, vao);
+	glGenBuffers(1, vbo);
+	glGenBuffers(1, ebo);
+
+	glBindVertexArray(*vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, *vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// xyz
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+	glBindVertexArray(0);
 }
 
 void pe_drawRect(pe_vec2 position, pe_vec2 size, pe_vec4 color){
